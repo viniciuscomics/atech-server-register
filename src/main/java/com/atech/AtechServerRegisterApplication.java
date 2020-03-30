0aba8f4647d238ff9034b84cf74067fa408c7d36
root <root@vinicius-Inspiron-3583>
2020-03-30 16:35:10 -0300

    Implementation of API Gateway and Eureka server

--- src/main/java/com/atech/AtechServerRegisterApplication.java
+++ src/main/java/com/atech/AtechServerRegisterApplication.java
@@ -2,7 +2,13 @@ package com.atech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
 public class AtechServerRegisterApplication {
 
